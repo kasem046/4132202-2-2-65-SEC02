331c6128c845f228616f7d687a9ed1e28d4584fb
muder <rong0951313818@gmail.com>
2022-12-02 11:59:21 +0700

    LAB1

---
+++ 640112418046/LAB4/php/index.php
@@ -0,0 +1,34 @@
+<?php 
+echo "Hello PHP"
+phpinfo()
+
+$num = 10:
+$name = "Win"$_COOKIE
+echo $ GET ['name'];
+echo $ POST ['post'];
+
+?>
+<table border="1">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td><?php echo $name ?></td>
+        </tr>
+        <?php
+        for()(&1 =2: $1 <5; $++){
+        ?>
+         <tr>
+            <td>2</td>
+            <td><?php $name?> Dum</td>
+         </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+</table>
